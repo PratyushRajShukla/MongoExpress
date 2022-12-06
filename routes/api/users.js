22cf70c01f1aa9f452db022f2cef7b1a63ee0d4e
PratyushRajShukla <pratyushraj.2089@gmail.com>
2022-12-06 17:56:05 +0530

    Adding a Working User Registeration ON DB Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -27,16 +28,40 @@ async (req,res) => {
 
     try{
         let user = await User.findOne({ email });
-
-        if(user){
-            res.status(400).json({errors: [{msg: 'User already exists'}]});
-        }
         
         //See if user exists
+
+         if(user){
+           return res.status(400).json({errors: [{msg: 'User already exists'}]});
+        }
+        
         //Get user gravatar
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+
         //Encrypt Password
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        
         //Return jsonwebtoken
 
+        res.send('User Registered');
+        
         res.send('User Route');
     } catch(err){
         console.error(err.message);
